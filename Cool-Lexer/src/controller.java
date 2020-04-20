4641d2929755fea7a903c61cac00660a6f4e542a
Mohamed Wagih <mhmedwagih1324@gmail.com>
2020-04-20 15:15:46 +0200

    Update controller.java

--- Cool-Lexer/src/controller.java
+++ Cool-Lexer/src/controller.java
@@ -16,81 +16,121 @@ public class controller {
     private List<Token> tokens;
     private List<Token> outErrors;
     private List<Token> outTokens;
-
+    /**
+     * This method creates an object of the controller.java class
+     */
     public static void main(String[] args) throws IOException{
         //to test with cmd
-        /*if(args.length >= 1){
-            controller lexicalObj = new controller(args[0]);
-        }
-        else{
-            System.out.println("Please input the name of the file!");
-        }*/
+            /*if(args.length >= 1){
+                controller lexicalObj = new controller(args[0]);
+            }
+            else{
+                System.out.println("Please input the name of the file!");
+            }*/
         //to test within the ide
         controller lexicalObj = new controller("good.cl");
     }
 
+    /**
+     * @param fileName
+     */
     public controller(String fileName) {
+        // start lexing the file
         startLexing(fileName);
     }
 
+    /**
+     * @param fileName
+     */
     public void startLexing(String fileName){
         CharStream charStream;
+        //get char stream from file
         try{
             charStream = CharStreams.fromFileName(fileName);
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
-
+        // create lexer from charStream
         lexer = new CoolLexer(charStream);
+        // create tokensStream from lexer
         tokensStream = new CommonTokenStream(lexer);
         tokensStream.fill();
+        // get tokens from tokensStream
         tokens = tokensStream.getTokens();
+        // two empty ArrayLists to save Tokens & errors
         outTokens = new ArrayList();
         outErrors = new ArrayList();
+        // get rule names of the lexer
         String[] ruleNames = lexer.getRuleNames();
+        // loop over the tokens
         for(Token token:tokens){                    //separating error tokens from out tokens
             if(token.getType()>0){      //means it'snot EOF
+                // get the type number of a token
                 int tokenTypeNUM = token.getType()-1;
+                // check for Error
                 if(ruleNames[tokenTypeNUM].equals("ERROR")){
+                    // if the rule name of the number is ERROR, add the error token to outErrors ArrayList
                     outErrors.add(token);
                 }
-                else{
+                else{// if not error, add the token to the outTokens ArrayList
                     outTokens.add(token);
                 }
             }
-        }
+        }   // finished separation
+        // Check for Errors
         if (!outErrors.isEmpty()){
+            // If there were Errors, print them
             printErrors();
         }
-        else{
+        else{// if there were no errors, write the tokens in file
             writetokens(fileName+"-lex");
         }
     }
 
+    /**
+     *
+     */
     public void printErrors(){
+        // create a string builder for the errors
         StringBuilder outString = new StringBuilder();
+        // loop over tokens that have errors
         for(Token error:outErrors){
+            // add the error data to the builder
             outString.append(
                     String.format("ERROR: %d: Lexer: invalid character: %s\n",
                             error.getLine(),error.getText()));
         }
+        // throw exception with the errors found
         throw new RuntimeException(outString.toString());
     }
 
+    /**
+     *
+     * @param outFile
+     */
     public void writetokens(String outFile){
         try {
+            // create a writer to write in a file
             BufferedWriter writer = new BufferedWriter(new FileWriter(outFile));
+            // get rule names of the lexer
             String[] ruleNames = lexer.getRuleNames();
+            // loop over tokens
             for (Token token : outTokens) {
+                // get the type number of a token
                 int tokenTypeNUM = token.getType()-1;
+                // write the line number of a token
                 writer.write(Integer.toString(token.getLine()) + "\n");
+                // write the token type name
                 writer.write(ruleNames[tokenTypeNUM] + "\n");
+                // check the token for specific type
                 if(ruleNames[tokenTypeNUM].equals("INTEGER") ||
                         ruleNames[tokenTypeNUM].equals("STRING") ||
                         ruleNames[tokenTypeNUM].equals("TYPE") ||
                         ruleNames[tokenTypeNUM].equals("IDENTIFIER"))
+                    // if it was of those types, print their text
                     writer.write(token.getText() + "\n");
             }
+            // close the file
             writer.close();
         } catch (IOException e) {
             throw new RuntimeException(e);
