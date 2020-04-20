e7d432ae7faac6da7028d2b8ed3348b8dfc3e28c
Mohamed Wagih <mhmedwagih1324@gmail.com>
2020-04-20 15:06:58 +0200

    Update ParseErrorListener.java

--- Cool-Parser/src/ParseErrorListener.java
+++ Cool-Parser/src/ParseErrorListener.java
@@ -4,9 +4,20 @@ import org.antlr.v4.runtime.Recognizer;
 import org.antlr.v4.runtime.misc.ParseCancellationException;
 
 public class ParseErrorListener extends BaseErrorListener {
+    /**
+     *
+     * @param recognizer
+     * @param offendingSymbol
+     * @param line
+     * @param charPositionInLine
+     * @param msg
+     * @param e
+     * @throws ParseCancellationException
+     */
     @Override
     public void syntaxError(Recognizer<?, ?> recognizer, Object offendingSymbol,int line,
                             int charPositionInLine, String msg, RecognitionException e) throws ParseCancellationException {
+        // throw an exception with data about the syntax error
         throw new ParseCancellationException("\nLine "+line+" : "+msg);
     }
 }
