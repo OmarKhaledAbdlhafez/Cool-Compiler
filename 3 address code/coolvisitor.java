411c4e3bc68a5123bb6d928066956b45b8bf0951
OmarKhaledAbdlhafez <43797108+OmarKhaledAbdlhafez@users.noreply.github.com>
2020-05-31 22:39:02 +0200

    Update coolvisitor.java

--- 3 address code/coolvisitor.java
+++ 3 address code/coolvisitor.java
@@ -1,5 +1,6 @@
 import org.antlr.v4.runtime.ParserRuleContext;
 public  coolvisitor  extends CoolRulesBaseVisitor {
+    int tcounts=0 , lcounts=0;
 @Override public Object visitProgram(CoolRulesParser.ProgramContext ctx) {
     return visitChildren(ctx);
 }
@@ -16,4 +17,65 @@ public  coolvisitor  extends CoolRulesBaseVisitor {
         System.out.println("    "+ ctx.IDENTIFIER()+" = " + visit(ctx.stmt())+";");
         return"";
 }
+  @Override
+    public Object visitExpressions(CoolRulesParser.ExpressionsContext ctx) {
+        String temp = "";
+        Object left = visit(ctx.stmt(0));
+        Object right = visit(ctx.stmt(1));
+        switch(ctx.getChild(1).getText()){
+            case "+":
+                temp = left +" + " + right ;
+                System.out.println("t" + tcount +" = " + temp + ";" );
+                tcount++;
+                break;
+            case "-":
+                temp = left + " - " + right;
+                System.out.println("t" + tcount +" = " + temp + ";" );
+                tcount++;
+                break;
+            case "*":
+                temp = left + " * " + right;
+                System.out.println("t" + tcount +" = " + temp + ";" );
+                tcount++;
+                break;
+            case "/" :
+                temp = left + " / " + right;
+                System.out.println("t" + tcount +" = " + temp + ";" );
+                tcount++;
+                break;
+            case "<":
+                temp = left + " < " +right;
+                System.out.println("t" + tcount +" = " + temp + ";" );
+                tcount++;
+                break;
+            case "<=":
+                temp = left + " <= " +right;
+                System.out.println("t" + tcount +" = " + temp + ";" );
+                tcount++;
+                break;
+            case "=":
+                temp = left + " == " +right;
+                System.out.println("t" + tcount +" = " + temp + ";" );
+                tcount++;
+                break;
+            default:
+                System.out.println("not valid operation ");
+
         }
+        return "t"+(tcount-1) ;
+    }
+      @Override
+    public Object visitLoops(CoolRulesParser.LoopsContext ctx) {
+
+        System.out.println("L"+lcount+":");
+        lcount++;
+        visit(ctx.stmt(0));
+        System.out.println("    IfZ  " +"t"+(tcount-1)+"  Goto  "+"L"+lcount+";");
+        visit(ctx.stmt(1));
+        System.out.println("Goto  "+"L"+(lcount-1)+";");
+        System.out.println("L"+lcount+":");
+
+        return "";
+    }
+        
+}
